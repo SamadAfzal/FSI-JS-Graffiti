faac6340b4e61375391e5eb1b18f9d180081a097
Samad <samadafzal1@g.ucla.edu>
2022-07-27 20:16:58 -0700

    did some of the work

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let header = document.querySelector('#page-header')
+header.style.textAlign= "right"
 
+...
+...
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.borderRadius = '50px'
+}
+
+let dogNames = document.body.("h1")
+body.style.textAlign= "left"
+
+let footer = document.querySelector('#page-footer')
+footer.style.textColor= "green"
 // Select some elements...
